32ad1a581bed5f53c60311cb14ea669d9f554e94
Vasiraja Murugan <vasiraja.murugan@tristha.com>
2026-03-27 15:40:45 +0530

    fixed

--- Feathers/src/services/chatserv/chatserv.class.ts
+++ Feathers/src/services/chatserv/chatserv.class.ts
@@ -17,19 +17,22 @@ export class ChatservService<ServiceParams extends Params = ChatservParams> exte
   ChatservParams,
   ChatservPatch
 > {
-  async setup(app: Application, path: string) {
-    app.service('chatserv').publish((data: any, context: any) => {
-      console.log('PUBLISH FIRED')
+  // async setup(app: Application, path: string) {
+  //   app.service('chatserv').publish((data: any, context: any) => {
+  //     console.log('PUBLISH FIRED')
 
-      const senderId = data.senderId?.toString()
-      const receiverId = data.receiverId?.toString()
+  //     const senderId = data.senderId?.toString()
+  //     const receiverId = data.receiverId?.toString()
 
-      const senderCh = app.channel(`msg/${senderId}`)
-      const receiverCh = app.channel(`msg/${receiverId}`)
+  //     const senderCh = app.channel(`msg/${senderId}`)
+  //     const receiverCh = app.channel(`msg/${receiverId}`)
 
-      return [senderCh, receiverCh]
-    })
-  }
+  //     return [senderCh, receiverCh]
+  //   })
+  // } 
+
+
+
 }
 
 export const getOptions = (app: Application): MongoDBAdapterOptions => {
